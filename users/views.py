cd2a2ca42956e5b718eecfb946d49f99ab91efe8
Graeme Harwood <131662338+Graeza@users.noreply.github.com>
2026-04-10 06:16:26 +0200

    Increase navbar logo size and responsiveness

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('products')
+
+    form = AuthenticationForm(request, data=request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        login(request, form.get_user())
+        return redirect('products')
+
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('products')
