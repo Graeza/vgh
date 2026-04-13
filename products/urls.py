f085ac0483ceb547222f70b2efe22821adbfe6d2
Graeme Harwood <131662338+Graeza@users.noreply.github.com>
2026-04-13 13:11:51 +0200

    Move checkout entry to cart and add cart quantity controls

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('products/', views.products, name='products-list'),
     path('products/<slug:slug>/', views.product, name='product'),
     path('cart/', views.cart, name='cart'),
+    path('cart/add/<uuid:product_id>/', views.add_to_cart, name='add-to-cart'),
+    path('cart/update/<uuid:product_id>/', views.update_cart_item, name='update-cart-item'),
     path('checkout/', views.checkout, name='checkout'),
 ]
