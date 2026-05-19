3d6039809144ad6a916c771b34b10907b5059bc0
Graeme Harwood <131662338+Graeza@users.noreply.github.com>
2026-05-19 06:34:28 +0200

    Add user order, address, and points database models

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,12 @@
-from django.contrib import admin 
-from .models import Profile, Message, Skill 
+from django.contrib import admin
+
+from .models import Address, Message, Order, PointLedger, Profile, Skill
+
 
 # Register your models here.
-admin.site.register(Profile) 
-admin.site.register(Message) 
+admin.site.register(Profile)
+admin.site.register(Message)
 admin.site.register(Skill)
+admin.site.register(Address)
+admin.site.register(Order)
+admin.site.register(PointLedger)
