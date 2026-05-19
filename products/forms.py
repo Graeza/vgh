59fcbe790a0d5077e6c242d0304be9d5e8d8b5c0
Graeme Harwood <131662338+Graeza@users.noreply.github.com>
2026-05-19 22:34:13 +0200

    Unify form styling with add-address form patterns

--- products/forms.py
+++ products/forms.py
@@ -21,3 +21,18 @@ class ProductForm(forms.ModelForm):
             'is_active',
             'featured_image',
         ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'input'}),
+            'sku': forms.TextInput(attrs={'class': 'input'}),
+            'category': forms.TextInput(attrs={'class': 'input'}),
+            'description': forms.Textarea(attrs={'class': 'input'}),
+            'stock_quantity': forms.NumberInput(attrs={'class': 'input'}),
+            'price': forms.NumberInput(attrs={'class': 'input'}),
+            'thc_min': forms.NumberInput(attrs={'class': 'input'}),
+            'thc_max': forms.NumberInput(attrs={'class': 'input'}),
+            'cbd_min': forms.NumberInput(attrs={'class': 'input'}),
+            'cbd_max': forms.NumberInput(attrs={'class': 'input'}),
+            'is_lab_tested': forms.CheckboxInput(attrs={'class': 'form-checkbox'}),
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-checkbox'}),
+            'featured_image': forms.ClearableFileInput(attrs={'class': 'input'}),
+        }
